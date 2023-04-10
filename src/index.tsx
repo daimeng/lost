0d2203a4654b6669c81294b1b45ce85972f0e999
Daimeng Chen <daimengchen@gmail.com>
2023-04-10 14:13:51 +0800

    border trees

--- src/index.tsx
+++ src/index.tsx
@@ -8,14 +8,37 @@ type Props = {
   grid: Array<Array<number>>
 }
 
-class MyComponent extends Component<Props, any> {
+class MyComponent extends Component<Props, { hidden: boolean }> {
+  constructor() {
+    super()
+
+    this.state = {
+      hidden: true
+    }
+  }
+
+  componentDidMount() {
+    document.addEventListener('keydown', this.toggleMap)
+  }
+
+  componentDidUnmount() {
+    document.removeEventListener('keydown', this.toggleMap)
+  }
+
+  toggleMap = (ev: KeyboardEvent) => {
+    if (ev.shiftKey && ev.key === 'Z') {
+      this.setState(s => ({ hidden: !s.hidden }))
+    }
+  }
+
   public render() {
     const { grid } = this.props
+    const { hidden } = this.state
 
     if (grid == null) return null
 
     return (
-      <div className="grid">
+      <div className={`grid ${hidden ? 'hidden' : ''}`}>
         {grid.map((row, i) =>
           <div className="grid-row">
             {row.map((borders, j) => {
